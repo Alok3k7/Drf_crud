557fc046608de4b3fd85e31108921cc53dd84a0c
Alok3k7 <aloknathtiwari2000@gmail.com>
2025-07-05 22:58:03 +0530

    bit of change in views.py

--- Drf_curd/views.py
+++ Drf_curd/views.py
@@ -38,7 +38,7 @@ def Drf_curd_detail(request, id):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
     elif request.method == "DELETE":
